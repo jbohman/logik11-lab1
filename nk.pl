28b1458964b3d487b2163ce4606978838231cf7c
Joel Bohman <joelboh@gmail.com>
2011-10-25 17:03:48 +0200

    on every matching rule remove rule from frp

--- nk.pl
+++ nk.pl
@@ -22,206 +22,224 @@ valid(_, [[_,_,assumption]|[]], _).
 
 % premise
 valid(Prems, [[X,Y,premise]|Rest], Frp):-
+    select([X,Y,premise], Frp, NewFrp),
     number(X),
     prop(Y),
     member(Y, Prems),
     %write('premise\n'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 
 % copy(x)
 valid(Prems, [[X,Y,Z]|Rest], Frp):-
+    select([X,Y,Z], Frp, NewFrp),
     number(X),
     prop(Y),
     Z = copy(A),
-    member([A,Y,_], Frp),
+    member([A,Y,_], NewFrp),
     %write('copy(x)\n'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 
 % andint(x,y)
 valid(Prems, [[X,Y,Z]|Rest], Frp):-
+    select([X,Y,Z], Frp, NewFrp),
     number(X),
     prop(Y),
     Z = andint(A,B),
-    member([A, C, _], Frp),
-    member([B, D, _], Frp),
+    member([A, C, _], NewFrp),
+    member([B, D, _], NewFrp),
     Y == and(C,D),
     %write('andint(x,y)\n'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 % andel1(x)
 valid(Prems, [[X,Y,Z]|Rest], Frp):-
+    select([X,Y,Z], Frp, NewFrp),
     number(X),
     prop(Y),
     Z = andel1(A),
-    member([A, B, _], Frp),
+    member([A, B, _], NewFrp),
     B = and(D, _),
     Y == D,
     %write('andel1(x)\n'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 % andel2(x)
 valid(Prems, [[X,Y,Z]|Rest], Frp):-
+    select([X,Y,Z], Frp, NewFrp),
     number(X),
     prop(Y),
     Z = andel2(A),
-    member([A, B, _], Frp),
+    member([A, B, _], NewFrp),
     B = and(_, E),
     Y == E,
     %write('andel2(x)\n'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 
 % orint1(x)
 valid(Prems, [[X,Y,Z]|Rest], Frp):-
+    select([X,Y,Z], Frp, NewFrp),
     number(X),
     prop(Y),
     Z = orint1(A),
     Y = or(B,C), % expand Y to or(B,C)
-    member([A, B, _], Frp), % do B exist
+    member([A, B, _], NewFrp), % do B exist
     prop(B), % is B a prop
     prop(C), % is C a prop
     %write('orint1(x)\n'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 % orint2(x)
 valid(Prems, [[X,Y,Z]|Rest], Frp):-
+    select([X,Y,Z], Frp, NewFrp),
     number(X),
     prop(Y),
     Z = orint2(A),
     Y = or(B,C), % expand Y to or(B,C)
-    member([A, C, _], Frp), % do C exist
+    member([A, C, _], NewFrp), % do C exist
     prop(B), % is B a prop
     prop(C), % is C a prop
     %write('orint2(x)\n'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 % orel(x,y,u,v,w)
 valid(Prems, [[X,Y,Z]|Rest], Frp):-
+    select([X,Y,Z], Frp, NewFrp),
     number(X),
     prop(Y),
     Z = orel(A,B,C,D,E),
-    member([A, F, _], Frp),
+    member([A, F, _], NewFrp),
     F = or(G,H),
-    member([B, G, assumption], Frp),
-    member([C, I, _], Frp),
-    member([D, H, assumption], Frp),
-    member([E, J, _], Frp),
+    member([B, G, assumption], NewFrp),
+    member([C, I, _], NewFrp),
+    member([D, H, assumption], NewFrp),
+    member([E, J, _], NewFrp),
     I == J,
     J == Y,
     %write('orel(x,y,u,v,w)\n'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 
 % impint(x,y)
 valid(Prems, [[X,Y,Z]|Rest], Frp):-
+    select([X,Y,Z], Frp, NewFrp),
     number(X),
     prop(Y),
     B is (X-1),
     Z = impint(A,B),
     Y = imp(C,D),
-    member([A,C,assumption], Frp),
-    member([B,D,_], Frp),
+    member([A,C,assumption], NewFrp),
+    member([B,D,_], NewFrp),
     %write('impint(x,y)\n'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 % impel(x,y)
 valid(Prems, [[X,Y,Z]|Rest], Frp):-
+    select([X,Y,Z], Frp, NewFrp),
     number(X),
     prop(Y),
     Z = impel(A,B),
-    member([B, imp(F,Y), _], Frp),
-    member([A, F, _], Frp),
+    member([B, imp(F,Y), _], NewFrp),
+    member([A, F, _], NewFrp),
     %write('impel(x,y)\n'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 
 % negint(x,y)
 valid(Prems, [[X,Y,Z]|Rest], Frp):-
+    select([X,Y,Z], Frp, NewFrp),
     number(X),
     prop(Y),
     Y = neg(E),
     Z = negint(A,B),
-    member([A, E, assumption], Frp),
-    member([B, cont, _], Frp),
+    member([A, E, assumption], NewFrp),
+    member([B, cont, _], NewFrp),
     %write('negint(x,y)\n'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 % negel(x,y)
 valid(Prems, [[X,Y,Z]|Rest], Frp):-
+    select([X,Y,Z], Frp, NewFrp),
     number(X),
     prop(Y),
     Y = cont,
     Z = negel(A,B),
-    member([A, C, _], Frp),
-    member([B, D, _], Frp),
+    member([A, C, _], NewFrp),
+    member([B, D, _], NewFrp),
     D == neg(C),
     %write('negel(x,y)\n'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 
 % contel(x) TODO
 valid(Prems, [[X,Y,Z]|Rest], Frp):-
+    select([X,Y,Z], Frp, NewFrp),
     number(X),
     prop(Y),
     Z = contel(A),
-    member([A, cont, _], Frp),
+    member([A, cont, _], NewFrp),
     %write('contel(x)'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 
 % negnegint(x)
 valid(Prems, [[X,Y,Z]|Rest], Frp):-
+    select([X,Y,Z], Frp, NewFrp),
     number(X),
     prop(Y),
     Z = negnegint(A),
-    member([A, B, _], Frp),
+    member([A, B, _], NewFrp),
     prop(B),
     Y == neg(neg(B)),
     %write('negnegint(x)\n'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 % negnegel(x)
 valid(Prems, [[X,Y,Z]|Rest], Frp):-
+    select([X,Y,Z], Frp, NewFrp),
     number(X),
     prop(Y),
     Z = negnegel(A),
-    member([A, B, _], Frp),
+    member([A, B, _], NewFrp),
     prop(B), % B == neg(neg(Y)) ?
     %write('negnegel(x)\n'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 
 % mt(x,y) TODO
 valid(Prems, [[X,neg(Y),Z]|Rest], Frp):-
+    select([X,neg(Y),Z], Frp, NewFrp),
     number(X),
     prop(Y),
     Z = mt(A,B),
-    member([A, imp(Y,D), _], Frp),
-    member([B, neg(D), _], Frp),
+    member([A, imp(Y,D), _], NewFrp),
+    member([B, neg(D), _], NewFrp),
     %write('mt(x,y)'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 
 % pbc(x,y)
 valid(Prems, [[X,Y,Z]|Rest], Frp):-
+    select([X,Y,Z], Frp, NewFrp),
     number(X),
     prop(Y),
     Z = pbc(A,B),
-    member([A, neg(Y), assumption], Frp),
-    member([B, cont, _], Frp),
+    member([A, neg(Y), assumption], NewFrp),
+    member([B, cont, _], NewFrp),
     %write('pbc(x,y)\n'),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
 % lem
 valid(Prems, [[X,Y,lem]|Rest], Frp):-
+    select([X,Y,lem], Frp, NewFrp),
     number(X),
     prop(Y),
     Y = or(A,neg(A)),
-    valid(Prems, Rest, Frp).
+    valid(Prems, Rest, NewFrp).
 
-% box?? should be assumption?
+% we are box
 valid(Prems, [Box|Rest], Frp):-
     is_box(Box),
     reverse(Box, RevBox),
@@ -230,7 +248,6 @@ valid(Prems, [Box|Rest], Frp):-
 
 is_box([[A, _, assumption]|_]):-
     number(A).
-
 
 flatten([],[]).
 flatten([[]|L],L).
